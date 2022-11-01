3c537260cfab207bde5a28c89ce4b8b7554ce039
dimkablin <311791@niuitmo.ru>
2022-11-02 02:10:08 +0300

    creating a question 383

--- src/main/java/RansomNote/Solution.java
+++ src/main/java/RansomNote/Solution.java
@@ -1,5 +1,5 @@
 package RansomNote;
 
-public class main {
+public class Solution {
 
 }
---
+++ src/main/java/RichestCustomerWealth/Solution.java
@@ -0,0 +1,21 @@
+package RichestCustomerWealth;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+        return 0;
+    }
+
+    public int[][] readMassive(Scanner scanner) {
+        Pattern pattern = Pattern.compile("");
+        String line;
+        while (scanner.hasNext()) {
+            line = scanner.nextLine();
+
+        }
+        return null;
+    }
+}
